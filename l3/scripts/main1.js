3e977f9de9ca18004f2fa8160cfad03284b19bfa
Vadim-tech-creator <vadikbond8190@gmail.com>
2024-08-02 09:50:37 +0300

    added l3

--- l3/scripts/main1.js
+++ l3/scripts/main1.js
@@ -1,9 +1,5 @@
-let year = 2024; 
-if (year % 400 === 0) {
-    console.log("Рік високосний");
-} else if (year % 100 === 0) {
-    console.log("Рік не є високосним");
-} else if (year % 4 === 0) {
+let year = 2000;
+if ((year % 400 === 0) || (year % 4 === 0 && year % 100 !== 0)) {
     console.log("Рік високосний");
 } else {
     console.log("Рік не є високосним");
